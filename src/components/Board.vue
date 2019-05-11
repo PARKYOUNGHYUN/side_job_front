561a183e762f6b3fc56746f1e184a64ded321b44
PARKYOUNGHYUN <bdt02295@naver.com>
2019-05-11 19:59:28 +0900

    create board component

---
+++ src/components/Board.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="grey lighten-3">
+    <v-card>
+      <v-container fluid grid-list-lg>
+        <v-layout row wrap>
+          <v-flex xs4 v-for="post in posts" :key="post.boardNo" @click="goToDetail(post.boardNo)">
+            <v-card color="cyan darken-2" class="white--text">
+              <v-layout row>
+                <v-flex xs12>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">{{ post.title }}</div>
+                      <div>{{ post.content }}</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-layout>
+                <v-flex xs5>
+                  <v-card-title primary-title>
+                    <v-icon>access_time</v-icon>　可能時間
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs7>
+                  <ul>
+                    <li v-for="time in post.progressTimes" :key="time.timeNo">
+                      {{ time.startAt | moment }} - {{ time.endAt | moment }}
+                    </li>
+                  </ul>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-card-actions class="pa-3" v-if="post.place">
+                <v-icon>place</v-icon>
+                <p v-for="place in post.place" :key="place">　{{ place }}</p>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  props: ['postType'],
+  data: () => {
+    return {
+      posts: []
+    }
+  },
+  created() {
+    this.createBoard();
+  },
+  methods: {
+    createBoard: function() {
+      this.$http.get('boards', {
+        params: {
+          postType: this.postType,
+          limit: 0
+        }
+      }).then((response) => {
+        this.makePosts(response.data)
+      }).catch((e) => {
+        console.error(e)
+      })
+    },
+    goToDetail: function(id) {
+      this.$router.push({
+        path: '/job/Detail/' + id
+      })
+    },
+    makePosts:  function(response) {
+      var list = []
+      response.map(function(value) {
+        var addrList = value.addressBook.reduce(function(a,b){
+          if (a.indexOf(b.addr1) < 0 ) a.push(b.addr1)
+          if (a.length > 2) {
+            a.push("...")
+            return a
+          } 
+          return a
+        },[])
+
+        var json = {
+          boardNo: value.boardNo,
+          title: value.title,
+          content: value.content,
+          place: addrList,
+          progressTimes: value.user.progressTimes
+        }
+        list.push(json)
+      })
+      this.posts = list
+    }
+  },
+  filters: {
+    moment: function (date) {
+        return moment(date).format('YYYY/MM/DD');
+    }
+  },
+  watch: {
+    postType: function () {
+      this.createBoard()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,74 +1,39 @@
 <template>
   <v-container grid-list-md>
-    <SearchBar/>
+    <SearchBar @childs-event="parentsMethod"/>
     <v-tabs color="cyan" dark slider-color="yellow">
-      <v-tab v-for="n in 2" :key="n" ripple @click="createBoard(n-1)">{{ tabTitle[n-1] }}</v-tab>
+      <v-tab v-for="n in 2" :key="n" ripple @click="changePostType(n-1)">{{ tabTitle[n-1] }}</v-tab>
       <v-tab-item v-for="n in 2" :key="n">
-        <v-data-table :headers="headers" :items="posts" item-key="name" class="elevation-1" hide-actions>
-          <template v-slot:headers="props">
-            <tr>
-              <th v-for="header in props.headers" :key="header.text">
-                {{ header.text }}
-              </th>
-            </tr>
-          </template>
-          <template v-slot:items="props">
-            <tr @click="goToDetail(props.item.boardNo)">
-              <td>{{ props.item.title }}</td>
-              <td>{{ props.item.content }}</td>
-              <td>{{ props.item.user.nickname }}</td>
-              <td>{{ props.item.entryStartAt }}</td>
-              <td>{{ props.item.entryEndAt }}</td>
-            </tr>
-          </template>
-        </v-data-table>
+        <Board :postType="postType"/>
       </v-tab-item>
     </v-tabs>
   </v-container>
 </template>
 
 <script>
+import moment from 'moment'
 import SearchBar from '@/components/SearchBar'
+import Board from '@/components/Board'
 
 export default {
   name: 'Top',
   components: {
-    SearchBar
+    SearchBar,
+    Board
   },
   data: () => {
     return {
-      headers: [
-        { text: 'タイトル', sortable: false, value: 'title' },
-        { text: '内容', sortable: false, value: 'content' },
-        { text: '作成者', sortable: false, value: 'user' },
-        { text: '応募開始時間', sortable: false, value: 'entry_start_at' },
-        { text: '応募終了時間', sortable: false, value: 'entry_end_at' }
-      ],
       tabTitle: ["求人", "求職"],
-      posts: []
+      postType: 0
     }
   },
-  created() {
-    this.createBoard(0);
-  },
   methods: {
-    createBoard: function(postType) {
-      this.$http.get('boards', {
-        params: {
-          postType: postType,
-          limit: 5
-        }
-      }).then((response) => {
-        this.posts = response.data
-        console.log(response.data)
-      }).catch((e) => {
-        console.error(e)
-      })
+    changePostType (postType) {
+      this.postType = postType
     },
-    goToDetail: function(id) {
-      this.$router.push({
-        path: '/job/Detail/' + id
-      })
+    parentsMethod: function(message) {
+      console.log('TEST')
+      console.log(message) // 자식으로 부터받은 메시지를 사용
     }
   }
 }

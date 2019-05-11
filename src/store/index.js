02e8e9d106105439ef15fead8166faaf0b3679df
PARKYOUNGHYUN <bdt02295@naver.com>
2019-05-11 14:45:06 +0900

    save login info in sessionStorage

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import getters from './getters'
 import actions from './actions'
 import mutations from './mutations'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -16,5 +17,11 @@ export default new Vuex.Store({
   state,
   mutations,
   getters,
-  actions
+  actions,
+  plugins: [
+    createPersistedState({
+      key: 'sub_job',
+      storage: window.sessionStorage,
+    })
+  ]
 })

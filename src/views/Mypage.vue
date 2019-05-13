0490d564c89682d300dbfbb90c3b55e2c02859e0
PARKYOUNGHYUN <bdt02295@naver.com>
2019-05-14 07:54:55 +0900

    modify mypage

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -1,40 +1,8 @@
 <template>
-  <div>
-    <v-tabs
-      v-model="active"
-      color="cyan"
-      dark
-      slider-color="yellow"
-    >
-      <v-tab
-        v-for="n in 2"
-        :key="n"
-        ripple
-      >
-        Item {{ n }}
-
-      </v-tab>
-      <v-tab-item
-        v-for="n in 2"
-        :key="n"
-      >
-        <v-card flat>
-          <v-card-text>{{ text }}</v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs>
-  </div>
+<p>MyPage</p>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        active: null,
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-      }
-    }
-  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
